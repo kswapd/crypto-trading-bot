e5823489fefa6c0154a488b10cb84f3dfc06a460
kongxiangwen <xw.kong@hnair.com>
2018-01-22 17:14:06 +0800

    minux view is ok

--- auto_trade.py
+++ auto_trade.py
@@ -122,9 +122,9 @@ class auto_trade(cv.console_view):
             all_coin = ('BTC', 'LTC', 'ETH', 'XRP', 'DASH', 'DOGE')
             for coin in all_coin:
                 cur = coin
-                prt_str = coin + " \t\t%7.2f \t%7.2f \t%7.2f, \t%7.2f, \t%7.2f \t%7.2f, \t%7.2f"%(self.p_info[cur]['last']['price'], self.k_info[cur]['last']['price'],self.k_info[cur]['last']['price']-self.p_info[cur]['last']['price'], (self.k_info[cur]['last']['price']-self.p_info[cur]['last']['price'])*100/self.p_info[cur]['last']['price'], self.y_info[cur]['last']['price'],self.y_info[cur]['last']['price']-self.p_info[cur]['last']['price'], (self.y_info[cur]['last']['price']-self.p_info[cur]['last']['price'])*100/self.p_info[cur]['last']['price'])
+                prt_str = coin + " \t\t%7.2f \t%7.2f \t%7.2f \t%7.2f \t%7.2f \t%7.2f \t%7.2f"%(self.p_info[cur]['last']['price'], self.k_info[cur]['last']['price'],self.k_info[cur]['last']['price']-self.p_info[cur]['last']['price'], (self.k_info[cur]['last']['price']-self.p_info[cur]['last']['price'])*100/self.p_info[cur]['last']['price'], self.y_info[cur]['last']['price'],self.y_info[cur]['last']['price']-self.p_info[cur]['last']['price'], (self.y_info[cur]['last']['price']-self.p_info[cur]['last']['price'])*100/self.p_info[cur]['last']['price'])
                 
-                prt_str =  re.sub(r'(-[\d+\.\d]+)','--', prt_str)   
+                prt_str =  re.sub(r'(-[\d+\.\d]+)','--\t', prt_str)   
                 stdscr.addstr(pos_x,pos_y,prt_str,curses.color_pair(3))
                 pos_x += 1
 
